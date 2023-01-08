db7d599cb86f59fbcb098773003de0f53ebb8424
Grace Lee <gracelee9901@gmail.com>
2023-01-08 15:58:59 -0800

    Update to use EF Core many-to-many database relationship

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ToDoList.Models
 {
@@ -6,6 +7,7 @@ namespace ToDoList.Models
   {
     public int ItemId { get; set; }
     public string Description { get; set; }
+    [Range(1, int.MaxValue, ErrorMessage = "You must add your item to a category. Have you created a category yet?")]
     public int CategoryId { get; set; }
     public Category Category { get; set; }
     public List<ItemTag> JoinEntities { get; }
